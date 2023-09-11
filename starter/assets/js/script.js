702113adc8432a28f642eee69697b04a1946a45b
andyvgumz <andyvgumz@yahoo.co.uk>
2023-09-11 22:53:35 +0100

    Daily Planner Modified

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,57 +1,60 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
     //Listen for save button clicks
 
-    $(".saveButton").on("click", function(){
-        console.log('It has been saved');
+    $(".saveButton").on("click", function (){
+        console.log(`It has been saved`);
 
-        let timeId = $(this).parent().attr("id")
+        let timeID = $(this).parent().attr("id")
         let value = $(this).siblings(".description").val();
 
-        console.log(timeId);
+        console.log(timeID);
         console.log(value);
 
-        localStorage.setItem(timeId, value);
+        localStorage.setItem(timeID, value);
 
         $(".notification").addClass('show');
 
-        setTimeout(function (){
-            $('.notifification').removeClass("show");
+        setTimeout(function () {
+            $(".notifification").removeClass("show");
         }, 5000)
     })
 
-    function hourUpdate(){
-        let currentHour = moment().hour();
-        console.log(currentHour);
+    function hourUpdate() {
+        let currentHour = moment().hours();
+        // console.log($(".time-block"));
+
+
+        for (let i = 0; i < $(".time-block").length; i++) {
+            let hour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1]);
+            console.log(hour)
+            console.log(currentHour)
+
+            if (hour < currentHour) {
+                $(".time-block")[i].classList.add("past")
+            } else if (hour === currentHour) {
+                $(".time-block")[i].classList.add("past")
+                $(".time-block")[i].classList.remove("present")
+            } else {
+                $(".time-block")[i].classList.remove("past")
+                $(".time-block")[i].classList.remove("present")
+                $(".time-block")[i].classList.add("future")
+            }
+        }
+
     }
 
     hourUpdate();
 
-    
+    let interval = setInterval(hourUpdate, 15000);
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("#currentDay").text(moment().format("dddd, MMMM, Do"))
 
 })
