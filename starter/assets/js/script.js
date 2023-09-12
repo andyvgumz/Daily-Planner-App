95148741a3108a5cd693cb1befaf91487fb201be
andyvgumz <andyvgumz@yahoo.co.uk>
2023-09-12 20:15:36 +0100

    Another Planner update

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
     //Listen for save button clicks
 
-    $(".saveButton").on("click", function (){
-        console.log(`It has been saved`);
+    $(".saveButton").on("click", function () {
+        console.log(`Appointment has been saved`);
 
         let timeID = $(this).parent().attr("id")
         let value = $(this).siblings(".description").val();
@@ -21,7 +21,7 @@ $(document).ready(function () {
     })
 
     function hourUpdate() {
-        let currentHour = moment().hours();
+        let currentHour = dayj(format).hourUpdate();
         // console.log($(".time-block"));
 
 
@@ -48,13 +48,29 @@ $(document).ready(function () {
 
     let interval = setInterval(hourUpdate, 15000);
 
+    $("#hour-0 .description").val(localStorage.getItem("hour-0"))
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"))
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"))
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"))
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"))
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"))
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"))
     $("#hour-9 .description").val(localStorage.getItem("hour-9"))
     $("#hour-10 .description").val(localStorage.getItem("hour-10"))
-    $("#hour-11 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
     $("#hour-13 .description").val(localStorage.getItem("hour-13"))
     $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
 
 
-    $("#currentDay").text(moment().format("dddd, MMMM, Do"))
 
+    $("#currentDay").text(dayjs().format("dd, D, MMM, YY"))
+
+
+ 
 })
